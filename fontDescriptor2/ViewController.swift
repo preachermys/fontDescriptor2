fd1c4bca63dabc6a6a3ae624a12877edd69690ad
Yulia Milovanova <preachermars@gmail.com>
2020-12-18 13:17:20 +0300

    Initial Commit

---
+++ fontDescriptor2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  fontDescriptor2
+//
+//  Created by Preacher on 18.12.2020.
+//  Copyright © 2020 Yulia Milovanova. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
